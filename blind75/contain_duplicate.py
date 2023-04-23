655596bc2169fac011fb62b1eab8b3bb7830ebab
indra aryadi <indraaryadikadek@gmail.com>
2023-04-23 19:51:50 -0400

    fixing code

--- blind75/contain_duplicate.py
+++ blind75/contain_duplicate.py
@@ -3,17 +3,21 @@
 # nums = [1,1,1,2,3,4,5]
 nums = [1, 4, 1, 5]
 
-nums_list = []
+# list vs set
+# difference time complexity when itterate and search using in operator is
+# list = O(n)
+# set = O(1)
+nums_set = set()
 
 
 def check_duplicate(nums):
     for i in nums:
-        if i in nums_list:
+        if i in nums_set:
             return True
         else:
-            nums_list.append(i)
+            nums_set.add(i)
     return False
 
 
 print(check_duplicate(nums))
-print(nums_list)
+print(nums_set)
